0a876d175bbe7e1e75d2c65029d9eb6e8f7bd9bd
Camilla Geber <camilla.geber@gmail.com>
2022-09-22 14:19:43 -0300

    trazendo novamente o arquivo errorMap. testes quebraram, inconsistência de avaliação nos testes

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,8 +1,9 @@
 const productsService = require('../services/productsService');
+const mapError = require('../utils/errorMap');
 
 const findProducts = async (_req, res) => {
   const { type, message } = await productsService.findProducts();
-  if (type) return res.status(type).json({ message });
+  if (type) return res.status(mapError(type)).json({ message });
     
     return res.status(200).json(message);
 };
@@ -11,7 +12,7 @@ const findProductsById = async (req, res) => {
   const { id } = req.params;
   const { type, message } = await productsService.findProductsById(id);
   
-  if (type) return res.status(type).json({ message });
+  if (type) return res.status(mapError(type)).json({ message });
 
   return res.status(200).json(message[0]);
 };
@@ -19,7 +20,7 @@ const findProductsById = async (req, res) => {
 const createProduct = async (req, res) => {
   const { type, message } = await productsService.createProduct(req.body);
 
-  if (type) return res.status(type).json({ message });
+  if (type) return res.status(mapError(type)).json({ message });
   
   return res.status(201).json(message);
 };
@@ -28,7 +29,7 @@ const updateProduct = async (req, res) => {
   const { id } = req.params;
   const upName = req.body;
   const { type, message } = await productsService.updateProduct(id, upName);
-  if (type) return res.status(type).json({ message });
+  if (type) return res.status(mapError(type)).json({ message });
   
   return res.status(200).json(message);
 };
@@ -36,7 +37,7 @@ const updateProduct = async (req, res) => {
 const deleteProduct = async (req, res) => {
   const { id } = req.params;
   const { type, message } = await productsService.deleteProduct(id);
-  if (type) return res.status(type).json({ message });
+  if (type) return res.status(mapError(type)).json({ message });
   res.status(204).end();
 };
 
--- src/services/productsService.js
+++ src/services/productsService.js
@@ -5,7 +5,7 @@ const PRODUCT_NOT_FOUND = 'Product not found';
 const findProducts = async () => {
   const result = await productsModel.findProducts();
   if (!result.length) {
-    return ({ type: 404, message: PRODUCT_NOT_FOUND });
+    return ({ type: 'PRODUCT_NOT_FOUND', message: PRODUCT_NOT_FOUND });
   }
   return { type: null, message: result };
 };
@@ -13,7 +13,7 @@ const findProducts = async () => {
 const findProductsById = async (id) => {
   const result = await productsModel.findProductsById(id);
   if (!result.length) {
-    return ({ type: 404, message: PRODUCT_NOT_FOUND });
+    return ({ type: 'PRODUCT_NOT_FOUND', message: PRODUCT_NOT_FOUND });
   }
   return { type: null, message: result };
 };
@@ -22,7 +22,7 @@ const createProduct = async (product) => {
   const id = await productsModel.createProduct(product);
 
   if (!id) {
-    return ({ type: 404, message: PRODUCT_NOT_FOUND });
+    return ({ type: 'PRODUCT_NOT_FOUND', message: PRODUCT_NOT_FOUND });
   }
 
   return { type: null, message: { id, ...product } };
@@ -31,7 +31,7 @@ const createProduct = async (product) => {
 const updateProduct = async (id, upName) => {
   const product = await productsModel.findProductsById(id);
   if (!product.length) {
-    return ({ type: 404, message: PRODUCT_NOT_FOUND });
+    return ({ type: 'PRODUCT_NOT_FOUND', message: PRODUCT_NOT_FOUND });
   }
   const result = await productsModel.updateProduct(id, upName);
   return { type: null, message: result };
@@ -40,7 +40,7 @@ const updateProduct = async (id, upName) => {
 const deleteProduct = async (id) => {
   const product = await productsModel.findProductsById(id);
   if (!product.length) {
-    return ({ type: 404, message: PRODUCT_NOT_FOUND });
+    return ({ type: 'PRODUCT_NOT_FOUND', message: PRODUCT_NOT_FOUND });
   }
   
   await productsModel.deleteProduct(id);
